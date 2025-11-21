bdb4cbeeeb5308111b4e9b1f14dd93cc1479c3e6
BinuMDX <dissanayakedmbm.20@itfac.mrt.ac.lk>
2025-11-21 17:12:30 +0530

    feat(chat-backend): add WebSocket support with Socket.IO

---
+++ chat-backend/src/chat/chat.controller.ts
@@ -0,0 +1,28 @@
+import { Controller, Get, Param, Post, Body } from '@nestjs/common';
+import { ChatService } from './chat.service';
+import { GetCurrentUserId } from '../auth/common';
+import { CreateMessageDto } from './dtos/create-message.dto';
+
+@Controller('chat')
+export class ChatController {
+   constructor(private chatService: ChatService) {}
+
+   @Get('/conversations')
+   getConversations(@GetCurrentUserId() userId: number) {
+      return this.chatService.getConversations(userId);
+   }
+
+   @Get('/messages/:conversationId')
+  getMessages(@Param("conversationId") id: string) {
+    return this.chatService.getMessages(id);
+  }
+
+  @Post('/messages/:conversationId')
+  sendMessage(
+    @Param('conversationId') conversationId: string,
+    @GetCurrentUserId() userId: number,
+    @Body() dto: CreateMessageDto,
+  ) {
+    return this.chatService.saveMessage(conversationId, userId, dto.content);
+  }
+}
