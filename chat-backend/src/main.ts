ad133c455fd99421d6724e90c69cae76a82be1d1
BinuMDX <dissanayakedmbm.20@itfac.mrt.ac.lk>
2025-11-24 00:46:36 +0530

    setup configurations to compatible with production

--- chat-backend/src/main.ts
+++ chat-backend/src/main.ts
@@ -6,7 +6,7 @@ import { AtGuard } from './auth/common/guards';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: 'https://chat-application-dws6.vercel.app/',
   });
   app.useGlobalPipes(new ValidationPipe());
   const reflector = new Reflector();
