4f4923af0eea4e8bd35c82f6cf6b668b2a6880ce
BinuMDX <dissanayakedmbm.20@itfac.mrt.ac.lk>
2025-11-24 10:45:35 +0530

    fix: error in cors origin

--- chat-backend/src/main.ts
+++ chat-backend/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
    app.enableCors({
-    origin: ['http://localhost:3000', 'https://chat-application-dws6.vercel.app/'],
+    origin: ['http://localhost:3000', 'https://chat-application-dws6.vercel.app'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
